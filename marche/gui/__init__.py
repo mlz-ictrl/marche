c86e2bbac6540168a4639b00266b1b065c711544
Alexander Lenz <alexander.lenz@frm2.tum.de>
2015-09-08 14:51:54 +0200

    Initial commit of the gui infrastructure.
    Change-Id: Ibae7a38ec217508aa67ba6c260e766b9c6367880

---
+++ marche/gui/__init__.py
@@ -0,0 +1,43 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# MLZ server control daemon
+# Copyright (c) 2015 by the authors, see LICENSE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Georg Brandl <g.brandl@fz-juelich.de>
+#   Alexander Lenz <alexander.lenz@frm2.tum.de>
+#
+# *****************************************************************************
+
+import sys
+
+import sip
+sip.setapi('QString', 2)
+sip.setapi('QVariant', 2)
+
+from PyQt4.QtCore import Qt
+from PyQt4.QtGui import QApplication
+
+def main():
+    app = QApplication(sys.argv)
+    app.setAttribute(Qt.AA_DontShowIconsInMenus, False)
+    app.setOrganizationName('mlz')
+    app.setApplicationName('march-gui')
+
+    ## win
+
+    return app.exec_()
