14ee2cf25951278be8c2f54a81ee8eaea3a89251
Georg Brandl <g.brandl@fz-juelich.de>
2015-09-09 11:51:53 +0200

    Fix debug leftover.

--- marche/jobs/entangle.py
+++ marche/jobs/entangle.py
@@ -116,15 +116,13 @@ class Job(BaseJob):
         if self._proc(name):
             raise Busy
         self.log.info('starting server %s' % name)
-        self._server_procs[name] = self._async(STARTING,
-                                               'sleep 5; %s start %s' % (INITSCR, name[9:]))
+        self._server_procs[name] = self._async(STARTING, '%s start %s' % (INITSCR, name[9:]))
 
     def stop_service(self, name):
         if self._proc(name):
             raise Busy
         self.log.info('stopping server %s' % name)
-        self._server_procs[name] = self._async(STOPPING,
-                                               'sleep 5; %s stop %s' % (INITSCR, name[9:]))
+        self._server_procs[name] = self._async(STOPPING, '%s stop %s' % (INITSCR, name[9:]))
 
     # XXX check devices with Tango clients
 
