569796bf61bbb54438b70bf5c6eca1894e4e00de
Georg Brandl <g.brandl@fz-juelich.de>
2018-03-10 10:46:18 +0100

    all: update copyrights
    Change-Id: I7ca3c8ef8e9bff36a32108f3476d6bade49b1dbd

--- marche/gui/scan.py
+++ marche/gui/scan.py
@@ -1,6 +1,6 @@
 #  -*- coding: utf-8 -*-
 # *****************************************************************************
-# Copyright (c) 2015-2017 by the authors, see LICENSE
+# Copyright (c) 2015-2018 by the authors, see LICENSE
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
